90ed546d82af83c411b91326eea2e7bb2f0940d9
SyoyaTanaka <hsa123syo@icloud.com>
2026-02-18 15:07:37 +0900

    たけなか　シーン切り替え8割完了（当社比）

---
+++ チーム制作project/Source/Scene/TutorialScene/TutorialScene.cpp
@@ -0,0 +1,41 @@
+#include "TutorialScene.h"
+#include "../../Input/Input.h"
+#include "../SceneManager.h"
+
+
+
+void InitTutorialScene()
+{
+
+}
+
+void LoadTutorialScene()
+{
+
+}
+
+void StartTutorialScene()
+{
+
+}
+
+void StepTutorialScene()
+{
+
+}
+
+void UpdateTutorialScene()
+{
+
+}
+
+void DrawTutorialScene()
+{
+
+}
+
+void FinTutorialScene()
+{
+
+
+}
---
+++ チーム制作project/Source/Scene/TutorialScene/TutorialScene.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "DxLib.h"
+
+void InitTutorialScene();
+void LoadTutorialScene();
+void StartTutorialScene();
+void StepTutorialScene();
+void UpdateTutorialScene();
+void DrawTutorialScene();
+void FinTutorialScene();
