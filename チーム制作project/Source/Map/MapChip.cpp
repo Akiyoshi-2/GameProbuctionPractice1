26cb45e0dafa73484bfb83529600c0333c1285a3
Shingo_takenaka <ktq2590314@stu.o-hara.ac.jp>
2026-02-20 10:55:07 +0900

    秋吉　Map追加

---
+++ チーム制作project/Source/Map/MapChip.cpp
@@ -0,0 +1,12 @@
+#include "DxLib.h"
+#include "MapChip.h"
+#include "Block.h"
+#include "MapParameter.h"
+#include "../GameSetting/GameSetting.h"
+
+MapChipData g_MapChip[MAP_CHIP_Y_NUM][MAP_CHIP_X_NUM] = { 0 };
+
+void LoadMapChipData()
+{
+
+}
