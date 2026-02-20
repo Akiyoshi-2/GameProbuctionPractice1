2f9dc94179988590481d2d826f94f26917d61431
Shingo_takenaka <ktq2590314@stu.o-hara.ac.jp>
2026-02-20 11:16:23 +0900

    ;

--- チーム制作project/Source/Map/MapChip.cpp
+++ チーム制作project/Source/Map/MapChip.cpp
@@ -3,6 +3,7 @@
 #include "Block.h"
 #include "MapParameter.h"
 #include "../GameSetting/GameSetting.h"
+#include "../Scene/TitleScene/TitleScene.h"
 
 MapChipData g_MapChip[MAP_CHIP_Y_NUM][MAP_CHIP_X_NUM] = { 0 };
 
