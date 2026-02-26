f39e92cce8f9e8993dd2564123d10f4808820ff0
SyoyaTanaka <hsa123syo@icloud.com>
2026-02-26 10:33:50 +0900

    竹中　ヘルメットの敵の続き書き終わり

--- チーム制作project/Source/Enemy/HelmetEnemy/HelmetEnemy.cpp
+++ チーム制作project/Source/Enemy/HelmetEnemy/HelmetEnemy.cpp
@@ -72,5 +72,211 @@ void LoadHelmetEnemy()
 	for (int i = 0; i < HELMET_ENEMY_MAX; i++)
 	{
 		g_HelmetEnemyData[i].animation[HELMET_ENEMY_ANIM_RUN].handle = runHandlde;
+		g_HelmetEnemyData[i].animation[HELMET_ENEMY_ANIM_DIE].handle = dieHandle;
 	}
+}
+
+void StepHelmetEnemy()
+{
+	HelmetEnemyData* helmet = g_HelmetEnemyData;
+	for (int i = 0; i < HELMET_ENEMY_MAX; i++, helmet++)
+	{
+		if (!helmet->active)continue;
+
+		g_PravHelmetEnemyData[i] = g_HelmetEnemyData[i];
+
+		helmet->move.x = 0.0f;
+
+		// 右に移動
+		if (!helmet->isTurn)
+		{
+			helmet->move.x = HELMET_ENEMY_MOVE_SPEED;
+		}
+		// 左に移動
+		else if (helmet->isTurn)
+		{
+			helmet->move.x = -HELMET_ENEMY_MOVE_SPEED;
+		}
+	}
+}
+
+void UpdateHelmetEnemy()
+{
+	HelmetEnemyData* helmet = g_HelmetEnemyData;
+	for (int i = 0; i < HELMET_ENEMY_MAX; i++, helmet++)
+	{
+		if (!helmet->active)continue;
+
+		helmet->pos.x += helmet->move.x;
+		helmet->pos.y += helmet->move.y;
+
+		UpdateHelmetEnemyAnimation(i);
+	}
+}
+
+void DrawHelmetEnemy()
+{
+	HelmetEnemyData* helmet = g_HelmetEnemyData;
+	for (int i = 0; i < HELMET_ENEMY_MAX; i++, helmet++)
+	{
+		if (!helmet->active)continue;
+
+		HelmetAnimationType animType = helmet->playAnim;
+		AnimationData* animData = &helmet->animation[animType];
+
+		if (!helmet->isTurn)
+		{
+			DrawAnimation(animData, helmet->pos.x, helmet->pos.y, FALSE, FALSE);
+		}
+	}
+}
+
+void FinHelmetEnemy()
+{
+	for (int i = 0; i < HELMET_ENEMY_ANIM_MAX; i++)
+	{
+		DeleteGraph(g_HelmetEnemyData->animation[i].handle);
+	}
+}
+
+HelmetEnemyData* GetHelmetEnemy()
+{
+	return g_HelmetEnemyData;
+}
+
+void PlayerKillHelmetEnemy(int index)
+{
+	HelmetEnemyData* helmet = &g_HelmetEnemyData[index];
+
+	helmet->active = false;
+
+	// スコア
+	// int score = GetScore() + HELMET_ENEMY_SCORE;
+	// SetScore(score)
+}
+
+void CreateHelmetEnemy(float posX, float posY, const EnemyParameter* param)
+{
+	HelmetEnemyData* helmet = g_HelmetEnemyData;
+	for (int i = 0; i < HELMET_ENEMY_MAX; i++, helmet++)
+	{
+		if (!helmet->active)
+		{
+			// アクティブ
+			helmet->active = true;
+
+			// アニメーション
+			helmet->animation->handle = g_HelmetEnemyData->animation->handle;
+
+			// 座標
+			helmet->pos.x = posX;
+			helmet->pos.y = posY;
+
+			// 移動量
+			helmet->move.x = HELMET_ENEMY_MOVE_SPEED;
+			helmet->move.y = 0.0f;
+
+			// パラメータ設定
+			helmet->param = param;
+
+			break;
+		}
+	}
+}
+
+void HelmetEnemyHitBlockX(MapChipData mapChipData, int index)
+{
+	HelmetEnemyData* helmet = &g_HelmetEnemyData[index];
+	BlockData* block = mapChipData.data;
+	const float POS_OFFSET = HELMET_ENEMY_COLLISION_OFFSET;
+	const float SIZE_OFFSET = HELMET_ENEMY_COLLISION_OFFSET * 2;
+
+	helmet->pos.x = g_HelmetEnemyData[index].pos.x;
+	helmet->pos.y = g_HelmetEnemyData[index].pos.y;
+
+	if (CheckSquareSquare(helmet->pos.x + POS_OFFSET, helmet->pos.y - POS_OFFSET,
+		HELMET_ENEMY_BOX_COLLISION_WIDTH - SIZE_OFFSET, HELMET_ENEMY_BOX_COLLISION_HEIGHT - SIZE_OFFSET,
+		block->pos.x, block->pos.y, MAP_CHIP_WIDTH, MAP_CHIP_HEIGHT))
+	{
+		// 左から当たった
+		if (helmet->move.x > 0.0f)
+		{
+			helmet->isTurn = false;
+		}
+		// 右から当たった
+		else if (helmet->move.x < 0.0f)
+		{
+			helmet->isTurn = true;
+		}
+
+	}
+}
+
+void HelmetEnemyHitBlockY(MapChipData mapChipData, int index)
+{
+	HelmetEnemyData* helmet = &g_HelmetEnemyData[index];
+	BlockData* block = mapChipData.data;
+	const float POS_OFFSET = HELMET_ENEMY_COLLISION_OFFSET;
+	const float SIZE_OFFSET = HELMET_ENEMY_COLLISION_OFFSET * 2;
+
+	helmet->isTurn = g_HelmetEnemyData->isTurn;
+
+	if (CheckSquareSquare(helmet->pos.x + POS_OFFSET, helmet->pos.y - POS_OFFSET,
+		HELMET_ENEMY_BOX_COLLISION_WIDTH - SIZE_OFFSET, HELMET_ENEMY_BOX_COLLISION_HEIGHT - SIZE_OFFSET,
+		block->pos.x, block->pos.y, MAP_CHIP_WIDTH, MAP_CHIP_HEIGHT))
+	{
+		// 上から当たった
+		if (helmet->move.y > 0.0f)
+		{
+			// 上に押し出す
+			helmet->pos.y -= (helmet->pos.y * HELMET_ENEMY_BOX_COLLISION_HEIGHT) - block->pos.y;
+			helmet->isAir = false;
+		}
+		// 下から当たったか
+		else if (helmet->move.y < 0.0f)
+		{
+			// 下に押し出す
+			helmet->pos.y += (block->pos.y + MAP_CHIP_WIDTH) - helmet->pos.y;
+		}
+
+		// Yの移動量を0にする
+		helmet->move.y = 0.0f;
+	}
+}
+
+void StartHelmetEnemyAnimation(HelmetAnimationType anim, int index)
+{
+	HelmetEnemyData* helmet = &g_HelmetEnemyData[index];
+
+	// アニメーション再生中なら何もしない
+	if (anim == helmet->playAnim)return;
+
+	// 再生中アニメーション設定
+	helmet->playAnim = anim;
+
+	// データを取得
+	AnimationData* animData = &helmet->animation[anim];
+	helmetEnemyAnimationParam animParam = HELMET_ENEMYANIM_PARAM[anim];
+
+	//再生
+	StartAnimation(animData, helmet->pos.x, helmet->pos.y,
+		animParam.interval, animParam.frameNum, animParam.width, animParam.height, true);
+}
+
+void UpdateHelmetEnemyAnimation(int index)
+{
+	HelmetEnemyData* helmet = &g_HelmetEnemyData[index];
+
+	// 歩くアニメーション
+	StartHelmetEnemyAnimation(HELMET_ENEMY_ANIM_RUN, index);
+
+	// 増えれば追加する
+	// StartHelmetEnemyAnimation(???, index);
+
+
+
+	// アニメーション更新
+	HelmetAnimationType animType = helmet->playAnim;
+	AnimationData* animData = &helmet->animation[animType];
+	UpdateAnimation(animData);
 }
