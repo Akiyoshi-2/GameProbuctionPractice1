e291865add70ae2dc9d2812d060f6e8d0543f9c9
DaikiTanaka <Daiki1117kirby0427@gmail.com>
2026-03-05 13:44:27 +0900

    消した

--- チーム制作project/Source/Scene/GameOverScene/GameOverScene.cpp
+++ チーム制作project/Source/Scene/GameOverScene/GameOverScene.cpp
@@ -6,20 +6,18 @@
 
 // GamOver文字画像
 int g_GameOverHandle = 0;
-// リザルト画像
-int g_ResultHandle = 0;
 
 void InitGameOver()
 {
 	g_GameOverHandle = 0;
 
-	g_ResultHandle = 0;
+	
 }
 
 void LoadGameOver()
 {
 	g_GameOverHandle = LoadGraph("Data/GameOver/画像/ゲームオーバー.png");
-	g_ResultHandle = LoadGraph("Data/GameOver/画像/result.png");
+	
 }
 
 void StartGameOver()
@@ -54,10 +52,7 @@ void DrawGameOver()
 	{
 		DrawGraph(250, 550, g_GameOverHandle, TRUE);
 	}
-	if (g_ResultHandle != -1)
-	{
-		DrawGraph(300, 450, g_ResultHandle, TRUE);
-	}
+	
 
 //	DrawUIText();
 }
@@ -65,5 +60,5 @@ void DrawGameOver()
 void FinGameOver()
 {
 	DeleteGraph(g_GameOverHandle);
-	DeleteGraph(g_ResultHandle);
+	
 }
